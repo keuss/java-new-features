8da440f33505bea8ff94002e59300e6693944af4
Guillaume GALLOIS <guillaume.gallois@gmail.com>
2020-03-25 15:22:06 +0100

    Update Features12Test.java

--- src/test/java/org/keuss/java12/Features12Test.java
+++ src/test/java/org/keuss/java12/Features12Test.java
@@ -12,8 +12,6 @@ public class Features12Test {
         final String attr = switch (day) {
             case 2, 3, 4, 5, 6 -> "weekday";
             case 7, 1 -> "weekend";
-            // it is possible to do this without a block and break
-            // so default -> "invalid"; is actually enough here
             default -> "invalid";
         };
         assertEquals("weekend", attr);
